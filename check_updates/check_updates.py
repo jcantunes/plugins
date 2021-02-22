5d36cce603b22611564d0771e7ac5b56f39a897f
João Antunes <jcantunes@gmail.com>
2021-02-22 06:07:01 +0000

    Added compatibility with DNF in check_updates

--- check_updates/check_updates.py
+++ check_updates/check_updates.py
@@ -11,7 +11,10 @@ data={}
 data['plugin_version'] = PLUGIN_VERSION
 data['heartbeat_required']=HEARTBEAT
 
-command="yum check-update --security | grep -i 'needed for security'"
+command_check_dnf="type dnf"
+command_yum="yum check-update --security | grep -i 'needed for security'"
+command_dnf_security="dnf --refresh -q check-update --security | sed '/Obsoleting Packages/,$d' | grep . | wc -l"
+command_dnf_all="dnf -q check-update | sed '/Obsoleting Packages/,$d' | grep . | wc -l"
 
 os_info = platform.linux_distribution()[0].lower()
 
@@ -22,19 +25,28 @@ def get_command_output(command):
     return output
 
 if 'centos' in os_info or 'red hat' in os_info:
-        out = get_command_output(command)
-        if out:
-            out = out.rstrip()
-            count = out.split("needed for security")
-            security_count = count[0].split()[0]
-            if security_count == 'No':
-                data['security_updates'] = 0
-            else:
-               data['security_updates'] = security_count
-            packages_count = count[1].split()
-            for each in packages_count:
-                 if each.isdigit():
-                     data['packages_to_be_updated']=each
+        dnf = get_command_output(command_check_dnf)
+        if dnf:
+            out = get_command_output(command_dnf_security)
+            if out:
+                data['security_updates'] = out.split()[0]
+            out = get_command_output(command_dnf_all)
+            if out:
+                data['packages_to_be_updated'] = out.split()[0]
+        else:
+            out = get_command_output(command_yum)
+            if out:
+                out = out.rstrip()
+                count = out.split("needed for security")
+                security_count = count[0].split()[0]
+                if security_count == 'No':
+                    data['security_updates'] = 0
+                else:
+                    data['security_updates'] = security_count
+                packages_count = count[1].split()
+                for each in packages_count:
+                    if each.isdigit():
+                        data['packages_to_be_updated']=each
                 
 else:    
     file_path='/var/lib/update-notifier/updates-available'
